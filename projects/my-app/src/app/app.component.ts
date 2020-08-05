bc848e42d3496e7c320ae8c42d613fef6ecbcf6a
mesantamaria <mesantamaria@uc.cl>
2020-08-05 11:09:00 -0400

    Example Application using library

---
+++ projects/my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <lib-my-lib></lib-my-lib>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+}
