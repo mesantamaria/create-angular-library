a7e6e88ab3829c7327f049767e6e862033285ef6
mesantamaria <mesantamaria@uc.cl>
2020-08-05 12:05:08 -0400

    Library built

--- projects/my-app/src/app/app.component.ts
+++ projects/my-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <lib-my-lib></lib-my-lib>
+    <lib-my-lib [name]="title"></lib-my-lib>
   `,
   styleUrls: ['./app.component.css']
 })
--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-my-lib',
   template: `
-    <p>
-      my-lib works!
-    </p>
+    <input type="text" (keydown.enter)="markText($event.target.value)">
+    <button>{{name}}</button>
   `,
   styles: [
   ]
 })
 export class MyLibComponent implements OnInit {
+  @Input() name;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  markText(value: any) {
+    console.log(value);
   }
 
 }
